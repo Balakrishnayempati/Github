0a9812f2f8fee173d57239da5f3b79880d4a1f88
Balakrishnayempati <balakrishnayempati007@gmail.com>
2024-01-10 15:50:53 +0530

    newone

---
+++ new.c
@@ -0,0 +1,9 @@
+# This is a simple Python program
+def main():
+    # Print 'Hello, World!' to the console
+    print("Hello, World!")
+
+# Execute the main function
+if __name__ == "__main__":
+    main()
+
